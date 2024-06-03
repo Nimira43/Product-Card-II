9a59f0d6a74035cb67fd0c5df2ea2878c15a7f29
Nick Rabone <n.rabone72@aol.com>
2024-06-03 01:50:17 +0100

    Developing file structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+import { Button } from "@/components/ui/button"
+
 function HomePage() {
   return (
-    <h1 className='text-3xl'>HomePage</h1>
+    <div>
+      <h1 className='text-3xl'>HomePage</h1>
+      <Button variant='outline' size='lg' className="uppercase m-8 text-[#ff4500]">
+        Submit
+      </Button>
+    </div>
+    
   )
 }
 export default HomePage
